9961bd90ab009b34b43368459aa32555a8f331da
rockabetty <marissafromtexas@gmail.com>
2023-10-25 12:20:07 -0700

    Creating a schema for users, sessions, roles, basic usage

---
+++ src/data/core_schema.sql
@@ -0,0 +1,45 @@
+CREATE TYPE user_role AS ENUM ('Member', 'Creator', 'Moderator');
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(250) UNIQUE NOT NULL,
+  email VARCHAR(250) UNIQUE NOT NULL,
+  password VARCHAR(512) NOT NULL,
+  password_reset_token VARCHAR(250) NOT NULL,
+  password_reset_expiry TIMESTAMP,
+  verified BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  role user_role DEFAULT 'Member'
+);
+
+CREATE TABLE IF NOT EXISTS users_sessions (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  session_token VARCHAR(250) UNIQUE NOT NULL,
+  session_id INT UNIQUE,
+  expiration_date TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS usage_devices (
+    id SERIAL PRIMARY KEY,
+    device_type VARCHAR(100),
+    browser VARCHAR(100),
+    os VARCHAR(100),
+    country VARCHAR(100),
+    language_locale VARCHAR(100)
+    created_at DATE DEFAULT CURRENT_DATE
+);
+
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = NOW();
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_users_modtime
+BEFORE UPDATE ON users FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
