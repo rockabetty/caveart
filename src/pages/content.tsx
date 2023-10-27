f344b494fd418e1a65618b67b32b6a7ab7ef7189
rockabetty <marissafromtexas@gmail.com>
2023-10-26 18:20:44 -0700

    Content policy

---
+++ src/pages/content.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import CaveartLayout from '../app/user_interface/CaveartLayout';
+
+function Content () {
+  return (
+    <CaveartLayout>
+      <h1>Content Policy</h1>
+
+      <p>
+        Caveart Webcomics is a platform that allows and encourages its users to express themselves through sequential art which may cover any number of genres and topics, as well as to curate their own reading experiences and communicate with fellow creators and readers.
+      </p>
+
+      <p>
+        The following standards are enforced by the admins.  Please abide by these rules not just by the letter of them but by their spirit as well.
+      </p>
+
+      <ul>
+        <li>
+          <b>Do not incite or promote violence, hate, or criminal acts.</b>  There is a difference between a comic that depicts bigotry, hate or violence and comics that encourage the same. 
+        </li>
+        <li>
+          <b>Your content should belong to you and you alone.</b>  Content that you upload which was created by another individual will be removed if it it was uploaded without the consent of the creator.
+        </li>
+        <li>
+          <b>Respect the privacy of others.</b>  Content containing personally identifying information will be removed.  Depending on severity, this content may be grounds for immediate removal even if it is a first-time offense. Dox somebody, get banned. Period.
+        </li>
+
+        <li>
+          <b>Do not post or encourage the posting of sexual or suggestive content involving minors.</b>  Doing so is grounds for banning on the first offense.
+        </li>
+
+        <li>
+          <b>Do not post or encourage the posting of beastiality content.</b> Content that depicts an animal (e.g. a feral) for the viewer's sexual gratification is beastiality content.  Sexual content that depicts anthropomorphized characters (not a feral) is not considered beastiality content.
+        </li>
+
+        <li>
+          <b>Keep it legal.</b>  Posting illegal content or solicit or facilitate illegal activity is grounds for an immediate ban from the website.
+        </li>
+
+        <li>
+          <b>Do not link to off-site content that violates our content policy.</b> We will remove attempts to use our platform to promote content that violates our policy.  We will curtail the permissions of users who repeatedly violate this rule.  If the off-site link is illegal content, we will ban you. If it is not illegal, we will not ban you. 
+        </li>
+      </ul>
+
+      <p>
+        We do not pro-actively monitor content uploaded to the platform.  Instead, we provide a reporting feature.  If you find material that is in violation of our content policy, please use our reporting feature to bring it to the attention of admins!
+      </p>
+
+      <p>We enforce our content policy the following ways depending on the circumstances:</p>
+      <ul>
+        <li>Asking you to remove the offending material within a time frame yourself before we remove it for you</li>
+        <li>Removing it for you and telling you not to ever do that again</li>
+        <li>Removing your account privileges if you continue to violate the policy in ways that do not result in a ban</li>
+        <li>Banning you for violating our TOS or the law</li>
+      </ul>
+    </CaveartLayout>
+  )
+}
+
+export default Content
