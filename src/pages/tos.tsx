196cfcb4fa5b7220421c509760b529c302471f8c
rockabetty <marissafromtexas@gmail.com>
2023-10-26 18:14:33 -0700

    Added TOS

---
+++ src/pages/tos.tsx
@@ -0,0 +1,140 @@
+import CaveartLayout from '../app/user_interface/CaveartLayout'
+
+function TermsofService() {
+ 
+  return (
+    <CaveartLayout>
+    <article>
+      <h1>Terms of Service</h1>
+      <h1>You must be at least 13 to have a Caveart account.</h1>
+      <p>
+        If we find out that you are under 13 we kick you out.
+      </p>
+
+      <p>
+        If you are using our website on the behalf of someone who isn't yourself (such as on the behalf of a co-author, a project-based friends group or comics collective) then you warrant that every single person that account represents is at least 13, end of story.
+      </p>
+
+      <h1>Invite people who will obey the TOS.</h1>
+      <p>
+        Much of our website is accessible through simply creating an account: you can browse comics, post public comments, and communicate with Admins by simply creating an account verified through email.
+      </p>
+
+      <p>
+        Being a comic artist, however, is invite only.  When you invite someone, we trust you to be inviting somebody who is going to obey our terms of service. If the person who you invited violates our terms of service in any way whatsoever, you both will have your privileges to invite new people permanently revoked.
+      </p>
+    
+      <h1>Privacy</h1>
+      <p>
+        Your use of our website indicates that you consent to our <a href="privacy">privacy policy</a> and its terms.
+      </p>
+
+      <h1>Content</h1>
+      <p>
+        You are responsible for any content you provide. Caveart is not responsible for the content created by its users.  We do not endorse, support, represent, or gurantee the completeness, truthfulness, accuracy, or reliability of any content that you post through our website.  You understand that by using our website you may be exposed to content that you may find offensive, harmful, triggering, deceptive, or otherwise inappropriate.  In these cases, please refer to our <a href="content">content policy</a> and make use of our reporting features. We will remove content that violates our <a href="content">content policy</a> and we will remove users who violtae our terms of service.  Even so, any and all content is the sole responsibility of the person who originated that content.
+      </p>
+
+      <p>
+      We reserve the right to remove content that violates our <a href="content">content policy</a>, from objectionable content to matters of copyright violation, impersonation and art theft.
+      </p>
+
+      <h2>Use the website as intended.</h2>
+      <p>
+        We reserve the right to remove content uploaded by people who are using the website in a manner contrary to its intended purpose as a comics hosting platform. Use of our platform as strictly a personal blog space, for example, just because we may offer a blogging feature, is an example of using the website in a manner that is not intended.
+      </p>
+
+      <h2>Adult content is allowed within reason.</h2>
+      <p>
+      When you create a comic, you will be able to designate material as 'adult' material such as sexual or violent content.  Failure to properly identify this content in your comic is grounds for removal of the content and/or the creator depending on each situation. Depending on the severity of the situation, you can be banned from Caveart with no notice and no second chances.  For example, uploading sexually explicit gore while simultaneously marking your comic as suitable for all audiences is a one-way ticket to an immediate ban.
+      </p>
+      <p>
+        Comics may contain sexual or sensual content but may not depict sexual or sensual acts involving minors.  Anybody under the age of 18 is a minor.  A character will be considered a minor based on their physical appearance. A childlike or youthful character will be considered a minor regardless of whether or not they're technically actually a 5,000 year old dragon child or whatever wild chronological/canonical age is in your story.
+      </p>
+
+      <p>
+        Comics may not depict bestiality. Anything depicting a human or anthropomorphic character engaging in a sexual act with an animal character will be considered bestiality. For you furries out there, that means sexualised or sensualized ferals are an absolute no-go. Other than that, yiff in heaven, furfriends, just actually mark sexual content as sexual content so that it can be hidden from users who do not want to (or should not) see it.
+      </p>
+
+      <p>
+        Comics may not promote or encourage violence or criminal acts in the real world, nor may they promote or encourage hostility based on identity or vulnerability. Please note, there is a difference between a comic that depicts bigotry, hate or violence and comics that <strong>encourage</strong> the same. The former is a part of writing and is a legitimate literary device. The latter is grounds for removal from the platform.  We particularly do not tolerate users engaging in the incitement of real-world violence, the denial or mockery of hate crimes (e.g. holocaust denial), or disparagement based on a particular characteristic (race, gender, ability, immigration status, etc). 
+      </p>
+
+      <h2>Forum, Chats, Comments And Messaging</h2>
+      <p>
+        Users have means by which to communicate with one another.  These rules apply to every communication medium provided by Caveart.
+      </p>
+      <p>
+        Comments become harassment the moment they are described as unwelcome and yet persist. When another user makes it clear that communication should stop, stop.  Continuing to contact that person in any way will be considered harassment and grounds for removal from the platform.  No exceptions will be made for people who claim to want to "just talk" or "heal this rift", etc., and no exceptions will be made for people who claim to be in a sympathetic situation (e.g., "They're my only friend!").  If your "only friend" blocked you, either you're manipulatively lying about how many friends you have to tug on our heart-strings, or you have a shitty friend who you should move on from.  In both situations, we still make no exceptions.
+      </p>
+      <p>Any behavior that is typically illegal regardless of where you live will be considered grounds for removal.  This includes but is not limited to the following smash hits:
+      </p>
+      <ul>
+        <li>Making a credible threat (as in what a police officer would deem a credible threat)</li>
+        <li>Making repeated or harrassing contact with an individual</li>
+        <li>Impersonation of another individual in order to mar their reputation</li>
+      </ul>
+
+      <p>
+        Behavior that is considered toxic is also grounds for removal. Providing a clear and consistent definition of  'toxic behavior' is not possible, but we still reserve the right to, frankly speaking, get rid of assholes.  Users who are at risk of removal will first be presented in writing with the exact behavior that needs to stop including examples of when it happened so they know precisely what it is they need to stop doing, first.  They will therefore be given the opportunity to stop and make amends.  If the behavior persists despite the intervention, that is when we will kick you out for being an asshole. 
+      </p>
+
+      <h2>Caveart may remove you for certain off-site behaviors</h2>
+
+      <p>
+        We desire to build an online community and we expect its members to adhere to our values. Therefore we do, in fact, take into account off-platform behavior in the ways described below.
+      </p>
+      <ul>
+        <li>
+          <strong>Revealing personally identifying information:</strong>  If you share personally identifying information  that you obtained through Caveart (for example, a naive user PMs you and tells you where they go to school) then we will ban you regardless of where, why, or how you share that information in order to no longer enable you.  Don&rsquo;t dox people.
+        </li>
+        <li>
+          <strong>Stalking:</strong> If a user draws a boundary with you (blocks you, tells you to stop talking to them) and you use another service to ignore that boundary, then we consider that stalking and we will therefore remove your access to our website's logged in features. We do not want to participate in somebody's continued efforts to disrespect someone else's boundaries.
+        </li>
+        <li>
+          <strong>Incitement to harrassment and violence:</strong> As we do not tolerate incitement of harrassment or violence, we will remove you for off-platform statements that encourage, instruct, or otherwise enable others to antagonize, malign, or physically harm other individuals.  Please note that while insulting commentary can be very distressing, we are under no obligation to take immediate action in response to insults unless they persist after intervention.  Please also note that while we will ban people who we deem to be a credible threat to your personal safety, we cannot actually protect your physical safety. If somebody else on the internet makes you feel unsafe you need to alert the proper authorities.
+        </li>
+      </ul>
+      <p>
+        The list above is not comprehensive.  If some unforeseen and extreme circumstance leads us to feel that we must ban somebody in the best interest of the rest of our users, then we will do so. 
+      </p>
+      
+      <p>
+        When removing users based on their behavior on other platforms, we will do our best to vet that these accounts correspond to the right people before taking decisive action.  We reserve the right to pre-emptively instate a temporary suspension of an account while we investigate a particular matter in order to potentially contain it.  That suspension will be lifted if we cannot be reasonably certain that an off-site account corresponds to a particular user. 
+      </p>
+
+      <h1>Your Rights</h1>
+      <p>
+        You represent and warrant that the content you post is actually yours or that you have all necessary permissions and consent that legally entitle you to to post the material.
+      </p>
+
+      <p>
+      You retain full rights to any content you submit, post, or display through our website.  Caveart will never attempt to assume ownership of your creations and anything that you used to create it (such as work in progress, photos, and so on).
+      </p>
+
+      <p>
+      By posting or displaying content on Caveart, you are granting us a worldwide, non-exclusive, royalty-free license (with the right to sublicense) to use, copy, reproduce, process, adapt, modify, publish, transmit, display and distribute your Content in any and all media or distribution methods.  What this means in plain English is that we have the right to do things like store your content on computers anywhere in the world and make as many backup copies as we need.  This is a normal part of making websites that host content.
+      </p>
+
+      <h1>Using The website</h1>
+      <p>
+        You agree not to abuse our website or attempt to compromise its security.  For instance, you agree not to use the website for malicious and illegal purposes such as stalking somebody or attempt to circumvent any security or authentication measures.  
+      </p>
+      <p>
+      We reserve the right to access, read, preserve, and disclose any information regarding your activity that is legal for us to keep in order to enforce the terms or investigate instances of malicious or illegal behavior. We do not disclose personally-identifying information to third parties. The only exception would be if we were forced to by law.
+      </p>
+
+      <h1>Your Account</h1>
+      <p>
+      You are responsible for safeguarding your account.  It is up to you to pick a strong password and keep it secret, keep it safe.
+      </p>
+
+      <p>
+      You consent to us contacting you through email in order to help you reset a forgotten password, provide a service announcement, or provide a security alert.  If the contact information you provide becomes no longer valid, it is up to you to update your contact information to help prevent us from communicating with the wrong person.
+      </p>
+
+    </article>
+    </CaveartLayout>
+  )
+}
+
+export default TermsofService;
