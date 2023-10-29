8bf99f0b4fe5b5e2982380bcc65fe2b9ee1ed84d
rockabetty <marissafromtexas@gmail.com>
2023-10-29 14:59:49 -0700

    Updating schema for comics

--- src/data/core_schema.sql
+++ src/data/core_schema.sql
@@ -1,4 +1,5 @@
 CREATE TYPE user_role AS ENUM ('Member', 'Creator', 'Moderator');
+CREATE EXTENSION ltree;
 
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
@@ -32,6 +33,103 @@ CREATE TABLE IF NOT EXISTS usage_devices (
     created_at DATE DEFAULT CURRENT_DATE
 );
 
+CREATE TABLE IF NOT EXISTS ratings (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    thumbnail TEXT
+);
+CREATE TABLE IF NOT EXISTS comics (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(250) UNIQUE NOT NULL,
+    subdomain VARCHAR(100) UNIQUE NOT NULL,
+    tagline VARCHAR(250),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    thumbnail TEXT,
+    comments BOOLEAN DEFAULT TRUE,
+    critique BOOLEAN DEFAULT FALSE,
+    is_unlisted BOOLEAN DEFAULT FALSE,
+    is_private BOOLEAN DEFAULT FALSE,
+    moderate_comments BOOLEAN DEFAULT FALSE,
+    view_count INT DEFAULT 0,
+    likes INT DEFAULT 0,
+    rating INT REFERENCES ratings(id),
+    stylesheet_variables JSONB,
+    UNIQUE (title, subdomain)
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT,
+    path_name LTREE
+);
+CREATE TABLE IF NOT EXISTS comics_to_genres (
+    id SERIAL PRIMARY KEY,
+    comic_id INT REFERENCES comics(id) ON DELETE CASCADE,
+    genre_id INT REFERENCES genres(id),
+    UNIQUE (comic_id, genre_id)
+);
+CREATE TABLE IF NOT EXISTS styles (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT,
+    path_name LTREE
+);
+CREATE TABLE IF NOT EXISTS comics_to_styles (
+    id SERIAL PRIMARY KEY,
+    comic_id INT REFERENCES comics(id) ON DELETE CASCADE,
+    style_id INT REFERENCES styles(id),
+    UNIQUE (comic_id, style_id)
+);
+CREATE TABLE IF NOT EXISTS content_warnings (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT,
+    parent_id INT references styles(id)
+);
+CREATE TABLE IF NOT EXISTS comics_to_content_warnings (
+    id SERIAL PRIMARY KEY,
+    comic_id INT REFERENCES comics(id) ON DELETE CASCADE,
+    content_warning_id INT REFERENCES content_warnings(id),
+    frequent BOOLEAN DEFAULT false,
+    UNIQUE (comic_id, content_warning_id)
+);
+CREATE TABLE IF NOT EXISTS comic_tags (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    comic_id INT REFERENCES comics(id)
+);
+CREATE TABLE IF NOT EXISTS chapters (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    index INT NOT NULL,
+    description TEXT NOT NULL,
+    comic_id INT REFERENCES comics(id),
+    thumbnail TEXT,
+    UNIQUE (comic_id, index)
+);
+CREATE TABLE IF NOT EXISTS comic_pages (
+    id SERIAL PRIMARY KEY,
+    page_number INT,
+    img TEXT NOT NULL UNIQUE,
+    comic_id INT REFERENCES comics(id) ON DELETE CASCADE,
+    chapter_id INT REFERENCES chapters(id),
+    created_at TIMESTAMP DEFAULT NOW(),
+    release_on TIMESTAMP DEFAULT NOW(),
+    UNIQUE (comic_id, chapter_id, page_number),
+    view_count INT DEFAULT 0,
+    like_count INT DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS comics_to_authors (
+    comic_id INT REFERENCES comics(id),
+    user_id INT REFERENCES users(id),
+    role TEXT,
+    UNIQUE (user_id, comic_id)
+);
+
+
 CREATE OR REPLACE FUNCTION update_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -43,3 +141,4 @@ $$ language 'plpgsql';
 CREATE TRIGGER update_users_modtime
 BEFORE UPDATE ON users FOR EACH ROW
 EXECUTE FUNCTION update_timestamp();
+
