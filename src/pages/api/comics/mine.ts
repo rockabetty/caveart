6d6979a5ac14b8aca3d5942e69731bceb1d431f2
rockabetty <marissafromtexas@gmail.com>
2024-01-10 19:45:05 -0800

    Typefixes for user comics page

--- src/pages/api/comics/mine.ts
+++ src/pages/api/comics/mine.ts
@@ -1,16 +1,16 @@
 import { NextApiHandler } from 'next';
 import { withAuth } from '../../../services/auth/server/withAuth';
-import { extractUserIdFromToken } from  '../../../services/auth/server/extractUserIdFromToken';
+import { extractUserIdFromToken } from  '../../../services/auth/server/extractUserIDFromToken';
 import { getComicsByAuthor } from '../../../data/comics';
 import { logger } from '../../../services/logs';
 
 const handler: NextApiHandler = async (req, res) => {
   try {
     const userID = await extractUserIdFromToken(req, false);
-    const comicsData = await getComicsByAuthor(userID);
+    const comicsData = await getComicsByAuthor(parseInt(userID));
     return res.status(200).send(comicsData);
   }
-  catch (error) {
+  catch (error: any) {
     logger.error(error);
     return res.status(500).send("Failed to fetch comic data");
   }
