46b984515c3fa0a1e822e7356534fd726907a92b
rockabetty <marissafromtexas@gmail.com>
2024-07-03 20:14:00 -0700

    reducing re-renders

--- src/services/auth/client/UserProvider.tsx
+++ src/services/auth/client/UserProvider.tsx
@@ -4,7 +4,7 @@
  * This component serves as a context provider for user authentication. It provides
  * functions for logging in, logging out, verifying a user, and viewing a user profile.
  * It manages the user authentication state using the userReducer.
- * 
+ *
  * If you want to use these functions, you actually are in the wrong place.  This file
  * defines how these functions act under the hood and are subject to change!  You want
  * to go to 'useUser', which is a more 'stable' file that exposes these functions in a
@@ -19,123 +19,141 @@
  * )
  */
 
-import {ReactNode, useReducer, useMemo} from "react";
+import { ReactNode, useReducer, useCallback, useMemo } from "react";
 import axios from "axios";
-import {useRouter} from "next/router";
-import {UserAuthState, UserAction, ActionType, UserContextType} from "../types/user";
+import { useRouter } from "next/router";
+import {
+    UserAuthState,
+    UserAction,
+    ActionType,
+    UserContextType,
+} from "../types/user";
 import UserContext from "./UserContext";
 import userReducer from "./hooks/userReducer";
-import {dev, loggerMap} from './hooks/userLogger';
+import { dev, loggerMap } from "./hooks/userLogger";
 import { ErrorLoggerPayload } from "../types/userlogger";
 
+const initialState: UserAuthState = {
+    user: null,
+    isAuthenticated: false,
+    isLoading: false,
+    authError: null,
+};
+
 interface UserProviderProps {
-    children: ReactNode;  /**< The React children components to be wrapped by the provider */
+    children: ReactNode /**< The React children components to be wrapped by the provider */;
 }
 
-const UserProvider: React.FC<UserProviderProps> = function({children}) {
-
-    const initialState: UserAuthState = {
-        user: null,
-        isAuthenticated: false,
-        isLoading: false,
-        authError: null,
-    };
-
-    const [state, _dispatch] = useReducer(userReducer, initialState);
-    const router = useRouter();
-
-    const handleError = (error: any) => {
-    const {stack} = error;
-    const {status} = error.response;
+const handleError = (dispatch: React.Dispatch<UserAction>) => (error: any) => {
+    const { stack } = error;
+    const { status } = error.response;
     const message = error?.response?.data?.error || error.message;
     const errorPayload: ErrorLoggerPayload = {
         message,
         stack,
-        status
-    }
-    dispatch({ type: ActionType.Error, payload: errorPayload });
+        status,
+    };
+    dispatch({
+        type: ActionType.Error,
+        payload: { authError: errorPayload },
+    });
 };
+
+const UserProvider: React.FC<UserProviderProps> = function ({ children }) {
+    const [state, _dispatch] = useReducer(userReducer, initialState);
+    const router = useRouter();
 
     /**
      * loggedDispatch dispatches actions with optional logging based on action type.
      * If the action type has a logFunction, the action payload will be logged.
      * If running in development mode and the action type is unknown, logs an error.
-     * 
+     *
      * An 'action payload' is an object with data that corresponds to the action.
      * That payload comes from the database. For example, a payload for viewing a
-     * user profile would be the profile data. 
-     * 
+     * user profile would be the profile data.
+     *
      * @param {UserAction} action - The action to be dispatched and logged.
      */
-    const dispatch = (action: UserAction) => {
-        const logFunction = loggerMap[action.type]
+    const dispatch = useCallback((action: UserAction) => {
+        const logFunction = loggerMap[action.type];
         if (logFunction) {
-            logFunction(action.payload)
+            logFunction(action.payload);
         } else if (dev) {
-           console.log(`Unknown action type: ${action.type}`);
+            console.log(`Unknown action type: ${action.type}`);
         }
         _dispatch(action);
-    }
+    }, []);
+
+    const handleErrorWithDispatch = useCallback(handleError(dispatch), [
+        dispatch,
+    ]);
 
     /**
      * loginUser will log in a user with provided email and password.
      * Dispatches loading state, sends API request, updates state accordingly,
      * and redirects to profile page on successful login.
-     * 
+     *
      * To 'dispatch' is to basically send out an alert to code that is listening
      * for certain actions. Once they 'hear' that alert, that's their 'cue' to do
      * whatever they're coded to do.
-     * 
+     *
      * So, there may be an interface that has a cute little loading graphic that
      * listens for the 'loading' state to dispatch before it shows the loading graphic.
-     * 
+     *
      * @param {string} email - The email of the user.
      * @param {string} password - The password of the user.
      */
-    const loginUser = async (email: string, password: string) => {
-        dispatch({ type: ActionType.Loading, payload: {process: ActionType.Login} });
-        try {
-            const loginInfo = { email, password };
-            const response = await axios.post('/api/auth/login', loginInfo);
+    const loginUser = useCallback(
+        async (email: string, password: string) => {
             dispatch({
-                type: ActionType.Login,
-                payload: response.data
+                type: ActionType.Loading,
+                payload: { process: ActionType.Login },
             });
-            router.push(`/profile`);
-        }
-        catch(error: any) {
-          handleError(error);
-        }
-    };
+            try {
+                const loginInfo = { email, password };
+                const response = await axios.post("/api/auth/login", loginInfo);
+                dispatch({
+                    type: ActionType.Login,
+                    payload: response.data,
+                });
+                router.push(`/profile`);
+            } catch (error: any) {
+                handleErrorWithDispatch(error);
+            }
+        },
+        [dispatch, router],
+    );
 
     /**
      * verifyUser will ask the server to verify if this user is logged in.
-     * When users log in, they're given authentication cookies (or tokens) 
-     * that get tucked away into the request headers. While our browser 
+     * When users log in, they're given authentication cookies (or tokens)
+     * that get tucked away into the request headers. While our browser
      * doesn't readily show these specific headers, the server can read them.
      * This function is basically asking the server, "Hey, do you recognize
      * this user from their cookie?"
-     * 
-     * We especially want to use this function for features that should 
-     * remain private to the user, like editing their own profile. It's 
-     * a way to make sure the person requesting access is genuinely who 
+     *
+     * We especially want to use this function for features that should
+     * remain private to the user, like editing their own profile. It's
+     * a way to make sure the person requesting access is genuinely who
      * they claim to be and not an imposter.
      */
-    const verifyUser = async () => {
-        dispatch({ type: ActionType.Loading, payload: {process: ActionType.Verify } });
+    const verifyUser = useCallback(async () => {
+        dispatch({
+            type: ActionType.Loading,
+            payload: { process: ActionType.Verify },
+        });
         axios.defaults.withCredentials = true;
         try {
-            const response = await axios.post('/api/auth/check');
-             dispatch({
+            const response = await axios.post("/api/auth/check");
+            dispatch({
                 type: ActionType.Verify,
-                payload: response.data
+                payload: response.data,
             });
             return response.data;
+        } catch (error: any) {
+            handleErrorWithDispatch(error);
         }
-        catch(error: any) {
-            handleError(error);
-        }
-    };
+    }, [dispatch]);
 
     /**
      * logoutUser will log the user out by clearing the authentication cookies (or tokens).
@@ -147,57 +165,61 @@ const UserProvider: React.FC<UserProviderProps> = function({children}) {
      * preventing it from making requests with old credentials.
      *
      */
-    const logoutUser = async () => {
-        dispatch({ type: ActionType.Loading, payload: {process: ActionType.Logout} });
+    const logoutUser = useCallback(async () => {
+        dispatch({
+            type: ActionType.Loading,
+            payload: { process: ActionType.Logout },
+        });
         try {
-            const response = await axios.post('/api/auth/logout');
+            const response = await axios.post("/api/auth/logout");
             dispatch({
                 type: ActionType.Logout,
-                payload: response.data
+                payload: response.data,
             });
-            router.push('/');
+            router.push("/");
+        } catch (error: any) {
+            handleErrorWithDispatch(error);
         }
-        catch(error: any) {
-            handleError(error);
-        }
-    };
+    }, [dispatch, router]);
 
     /**
      * viewProfile retrieves the user's profile data from the server and
-     * returns it as an object. The function doesn't take any arguments 
-     * because it relies on the server's knowledge of who is logged in. 
-     * The server identifies the logged-in user through authentication 
+     * returns it as an object. The function doesn't take any arguments
+     * because it relies on the server's knowledge of who is logged in.
+     * The server identifies the logged-in user through authentication
      * tokens or cookies set during the login process. As such, there's no
      * need for us to specify which user's profile to fetch since the server
      * already knows.
      */
-    const viewProfile = async () => {
-        dispatch({ type: ActionType.Loading, payload: {process: ActionType.ViewProfile} });
+    const viewProfile = useCallback(async () => {
+        dispatch({
+            type: ActionType.Loading,
+            payload: { process: ActionType.ViewProfile },
+        });
         try {
             const response = await axios.get(`/api/auth/profile`);
             dispatch({
                 type: ActionType.ViewProfile,
-                payload: response.data
+                payload: response.data,
             });
             return response.data;
+        } catch (error: any) {
+            handleErrorWithDispatch(error);
         }
-        catch(error: any) {
-            handleError(error);
-        }
-    }
+    }, [dispatch]);
 
     /**
      * useMemo is a hook (a certain type of function specific to React) which
      * 'memorizes' the result of a complicated function so it doesn't have to
      * re-compute that function every time it turns around.  Technically it's
-     * 'memoizing', not 'memorizing', but for colloquial purposes, think of it 
+     * 'memoizing', not 'memorizing', but for colloquial purposes, think of it
      * as memorizing things.
-     * 
-     * Normally React's render function will fire off every single time the 
+     *
+     * Normally React's render function will fire off every single time the
      * slightest thing changes.  Faster than your eyes can see, it will dump
      * the screen and re-calculate the screen and re-draw that screen, and
      * it doesn't matter if the change that results from it is imperceptible.
-     * 
+     *
      * UseMemo takes a way more lax approach. It basically tells the browser,
      * "Wake me up when something actually interesting happens. If it isn't
      * interesting, I'm spitting out the same thing I gave you last time."
@@ -205,32 +227,37 @@ const UserProvider: React.FC<UserProviderProps> = function({children}) {
      * What is 'interesting' to useMemo is the dependency array.
      * Here it's '[state, dispatch]', so that the big "UserContextType" array
      * doesn't get re-created every time some unrelated data changes
-     * 
+     *
      * See 'UserContext' for what exactly is going on in the array passed
      * to this useMemo function call.
      */
     const contextValue = useMemo(
-        () => [state,
-        dispatch,
-        verifyUser,
-        loginUser,
-        logoutUser,
-        viewProfile] as UserContextType,
-        [state, dispatch]
+        () =>
+            [
+                state,
+                dispatch,
+                verifyUser,
+                loginUser,
+                logoutUser,
+                viewProfile,
+            ] as UserContextType,
+        [state, dispatch, verifyUser, loginUser, logoutUser, viewProfile],
     );
 
     /**
      * UserContext.Provider is the provider component for the UserContext.
      * By wrapping parts of your component tree (your HTML elements) with this
-     * provider, you make the contextValue available to any nested component, 
+     * provider, you make the contextValue available to any nested component,
      * without having to pass props down manually at every level.
      *
      * UserContext.Provider takes the contextValue as its value property.
-     * Any child component inside this provider can now use the React useContext 
+     * Any child component inside this provider can now use the React useContext
      * hook to access the values and functions defined in contextValue.
      */
     return (
-        <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+        <UserContext.Provider value={contextValue}>
+            {children}
+        </UserContext.Provider>
     );
 };
 
