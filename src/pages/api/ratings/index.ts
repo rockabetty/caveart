29a7e4d4f7a670972ae11600dd43c23a13b9734f
rockabetty <marissafromtexas@gmail.com>
2024-07-05 00:45:50 -0700

    fixing ratings handler when no key query param

--- src/pages/api/ratings/index.ts
+++ src/pages/api/ratings/index.ts
@@ -2,19 +2,16 @@ import { NextApiHandler } from 'next';
 import { getRatingDefs } from '../../../data/comics';
 
 const handler: NextApiHandler = async (req, res) => {
-
   const {key} = req.query;
-  if (key === 'name' || key === 'id') {
-    try {
-      const ratings = await getRatingDefs(key);
-      return res.status(200).send(ratings);
-    } catch (error) {
-      return res.status(500).send(error)
-    }
-  } else {
+  if (!!key && (key !== 'name' || key !== 'id')) {
     return res.status(400).send(new Error());
   }
-  
+  try {
+    const ratings = await getRatingDefs(key);
+    return res.status(200).send(ratings);
+  } catch (error: any) {
+    return res.status(500).send(error)
+  }
 }
 
 export default handler;
