61908790e051f87aa9f92b366944a6fa3615e16a
rockabetty <marissafromtexas@gmail.com>
2024-07-18 11:30:28 -0700

    removing auth svc now that it's nice and restructured

--- src/services/auth/server/api/checkAuth.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next';
-import { withAuth } from '../withAuth';
-
-const handler: NextApiHandler = async (_req, res) => {
-  return res.status(200).json({ authenticated: true })
-};
-
-export default withAuth(handler);
--- src/services/auth/server/api/index.ts
+++
@@ -1,7 +0,0 @@
-export {default as signupHandler} from './signup';
-export {default as profileHandler} from './profile';
-export {default as logoutHandler} from './logout';
-export {default as loginHandler} from './login';
-export {default as checkAuthHandler} from './checkAuth';
-export {default as requestPasswordResetHandler} from './requestPasswordReset';
-export {default as resetPasswordHandler} from './resetPassword';
