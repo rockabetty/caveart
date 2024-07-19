3a3878909af556216d59074285d185bb56210266
rockabetty <marissafromtexas@gmail.com>
2024-07-18 23:06:07 -0700

    adding extractUserIdFromToken

---
+++ src/server/domains/users/outbound/extractUserIdFromToken.ts
@@ -0,0 +1,42 @@
+/**
+ * Checks a JWT from request headers and returns an ID based on decoding the sub.
+ * 
+ * This function optionally checks a token for validity, in case it needs to be 
+ * called within an endpoint that is already checking for validity but needs to
+ * know the actual user ID number for other purposes like passing user_id to bridge
+ * tables.  The default assumption is to validate the token. 
+ * 
+ * @param {NextApiRequest} req - The request from which headers will be read
+ * @param {boolean} validateSession - Whether to validate the session, default true
+ * @returns {string} A string containing the user ID, derived from the JWT's sub.
+ * @throws Throws an error if decoding the token, getting user session, or any other operation fails.
+ *
+ */
+import { NextApiRequest } from 'next';
+import jwt from 'jsonwebtoken';
+import { getUserSession } from './userRepository';
+import { requireEnvVar } from '@logger/envcheck';
+import { ErrorKeys } from '../errors.types';
+
+const SECRET_KEY_JWT = requireEnvVar('SECRET_KEY_JWT');
+const USER_AUTH_TOKEN_NAME = requireEnvVar('NEXT_PUBLIC_USER_AUTH_TOKEN_NAME');
+
+export async function extractUserIdFromToken (req: NextApiRequest, validateSession: boolean = true): Promise<string> {
+  const token = req.cookies[USER_AUTH_TOKEN_NAME];
+  if (token) {
+    const decodedRequestToken = jwt.verify(token, SECRET_KEY_JWT);
+    if (validateSession) {
+      const userSession = await getUserSession(token);
+      if (!userSession || decodedRequestToken.sub !== userSession.user_id.toString()) {
+        throw new Error(ErrorKeys.SESSION_INVALID);
+      }
+    }
+
+    return decodedRequestToken.sub as string
+  }
+  
+  throw new Error(ErrorKeys.SESSION_INVALID)
+   
+}
+
+export default extractUserIdFromToken;
