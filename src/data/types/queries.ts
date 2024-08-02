82fd51aad594ff42fd6ed5b621bb453d280f5378
rockabetty <marissafromtexas@gmail.com>
2024-08-01 21:19:38 -0700

    removing the needless data folder, whee!

--- src/data/types/queries.ts
+++
@@ -1,7 +0,0 @@
-import { PoolClient, QueryResult } from "pg";
-
-export type QueryFunction = (client: PoolClient) => Promise<QueryResult>;
-
-export type GenericStringMap = {
-  [key:string]: string | string[] | number[] | number | Date | boolean | null;
-};
