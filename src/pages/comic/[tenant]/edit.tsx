4ea25e806f5d03f9ba664f5e275af34ed9c353fb
rockabetty <marissafromtexas@gmail.com>
2024-11-15 15:10:22 -0800

    cosmetic changes

--- src/pages/comic/[tenant]/edit.tsx
+++ src/pages/comic/[tenant]/edit.tsx
@@ -1,24 +1,23 @@
-import CaveartLayout from '../../../app/user_interface/CaveartLayout'
-import EditComicProfile from '../../../app/user_interface/comic/profiles/EditComicProfile';
-import ComicProfileProvider from '../../../app/user_interface/comic/profiles/hooks/ComicProfileProvider'
-import { useRouter } from 'next/router';
-import { useTranslation} from 'react-i18next';
+import CaveartLayout from "@features/CaveartLayout";
+import EditComicProfile from "@features/comic/profiles/EditComicProfile";
+import ComicProfileProvider from "@features/comic/profiles/hooks/ComicProfileProvider";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
 
 function EditComic() {
-
   const { t } = useTranslation();
   const { tenant } = useRouter().query;
-  
+
   return (
-  <CaveartLayout requireLogin={true}>
-    <h1>{t('comicManagement.edit')}</h1>
-      <ComicProfileProvider>
-        <EditComicProfile
-          tenant={tenant}
-        />
-      </ComicProfileProvider>
-   </CaveartLayout>
-  )
+    <CaveartLayout requireLogin={true}>
+      <div className="tile">
+        <h1>{t("comicManagement.edit")}</h1>
+        <ComicProfileProvider>
+          <EditComicProfile tenant={tenant} />
+        </ComicProfileProvider>
+      </div>
+    </CaveartLayout>
+  );
 }
 
 export default EditComic;
